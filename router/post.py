229753eda904b468b6f39f463d82e562f275fc5f
Muhammad Abdullah <whello544@gmail.com>
2026-07-01 18:05:39 +0500

    Update post.py

--- router/post.py
+++ router/post.py
@@ -25,3 +25,35 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), curren
 def get_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
     return {"data": posts}  
+
+##
+
+@router.get("/{id}")
+def get_post(id: int, db: Session = Depends(get_db),current_user: int = Depends(oauth2.get_current_user)):
+    post = db.query(models.Post).filter(models.Post.id == id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"post with id: {id} was not found")
+    return {"post_detail": post}
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+    post_query = db.query(models.Post).filter(models.Post.id == id)    
+    post = post_query.first()
+    if post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id {id} does not exist")
+    post_query.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@router.put("/{id}")
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db),current_user: int = Depends(oauth2.get_current_user)):    
+    post_query = db.query(models.Post).filter(models.Post.id == id)    
+    post = post_query.first()
+    if post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id {id} does not exist")    
+    post_query.update(updated_post.dict(), synchronize_session=False)
+    db.commit()    
+    return {"data": post_query.first()}
