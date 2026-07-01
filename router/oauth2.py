886b4d35de844a489774d2e09489d4ee20a4a415
Muhammad Abdullah <whello544@gmail.com>
2026-07-01 18:08:44 +0500

    Implement JWT access token creation and verification
    Added functions to create and verify access tokens using JWT.

--- router/oauth2.py
+++ router/oauth2.py
@@ -11,3 +11,23 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+def create_access_token(data):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(
+    minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+)
+    to_encode.update({"exp" : expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def verify_access_token(token:str, credentials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        id:str = payload.get("user_id")
+        if id is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(id=id)
+    except JWTError:
+        raise credentials_exception
+    return token_data
