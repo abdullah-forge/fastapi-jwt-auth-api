bd9318457ddea08ed074487ebb0310f89fbf787c
Muhammad Abdullah <whello544@gmail.com>
2026-07-01 18:10:44 +0500

    Update auth.py

--- router/auth.py
+++ router/auth.py
@@ -15,3 +15,28 @@ def login(
     user_credentials: OAuth2PasswordRequestForm = Depends(),
     db: Session = Depends(get_db)
 ):
+
+    user = db.query(models.User).filter(
+        models.User.email == user_credentials.username
+    ).first()
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Invalid credentials"
+        )
+
+    if not utilis.verify(user_credentials.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Invalid credentials"
+        )
+
+    access_token = oauth2.create_access_token(
+        data={"user_id": user.id}
+    )
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
