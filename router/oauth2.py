ddd1cc3ed3c30672e3c45dba037bc011ef64c6b2
Muhammad Abdullah <whello544@gmail.com>
2026-07-01 18:09:13 +0500

    Add get_current_user function for user retrieval

--- router/oauth2.py
+++ router/oauth2.py
@@ -31,3 +31,10 @@ def verify_access_token(token:str, credentials_exception):
     except JWTError:
         raise credentials_exception
     return token_data
+
+def get_current_user(token:str = Depends(oauth2_scheme), db : Session = Depends(database.get_db)):
+    credential_exception = HTTPException(status_code = status.HTTP_401_UNAUTHORIZED,detail = f"Could not validate credentials",
+                                         headers = {"WWW-Authenticate":"Bearer"})
+    token = verify_access_token(token, credential_exception)
+    user = db.query(models.User).filter(models.User.id == token.id).first()
+    return user
