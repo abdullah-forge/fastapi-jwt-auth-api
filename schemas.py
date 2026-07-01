f6053bc249578dc27dea1ed7344db2b905da7444
Muhammad Abdullah <whello544@gmail.com>
2026-07-01 18:13:48 +0500

    Add UserOut, Token, and TokenData models

--- schemas.py
+++ schemas.py
@@ -16,3 +16,20 @@ class PostCreate(PostBase):
 class UserCreate(BaseModel):
     email : EmailStr
     password : str
+
+class UserOut(BaseModel):
+    id : int
+    email : EmailStr
+    created_at : datetime
+    class Config:
+        from_attributes = True
+class UserLogin(BaseModel):
+    email : EmailStr
+    password : str
+
+class Token(BaseModel):
+    access_token : str
+    token_type : str
+
+class TokenData(BaseModel):
+    id : Optional[str] = None
