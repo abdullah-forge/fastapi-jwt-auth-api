020943e3e49335b4f3b954c1e1937276dbe16eb0
Muhammad Abdullah <whello544@gmail.com>
2026-07-04 18:52:31 +0500

    Update SQLALCHEMY_DATABASE_URL configuration
    Refactor database URL to use settings from config.

--- database.py
+++ database.py
@@ -2,7 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Admin123@localhost/postgres"
+from .config import settings
+
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql://{settings.database_username}:"
+    f"{settings.database_password}@"
+    f"{settings.database_hostname}:"
+    f"{settings.database_port}/"
+    f"{settings.database_name}"
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
